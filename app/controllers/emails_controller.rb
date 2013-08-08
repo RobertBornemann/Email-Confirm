a67314004faca7296b0736370ee568360fa1523e
Jason Green <jason.green@wimdu.com>
2013-08-08 14:14:50 +0200

    a'

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,15 +1,22 @@
 class EmailsController < ActionController::Base
+
+  http_basic_authenticate_with name: "robert", password: "roberto", only: :index
+
   def new
     @email = Email.new(email: params[:email])
     if @email.save
-      redirect_to emails_path
+      redirect_to email_path(@email)
     end
   end
 
 
 
   def index
-    
+    @emails = Email.all
+  end
+
+  def show
+
   end
 end
 
