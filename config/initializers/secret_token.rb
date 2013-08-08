a76e3ee003c8117c439a5cf302e2b9e5f1771a23
Jason Green <jason.green@wimdu.com>
2013-08-08 12:40:53 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emailconfirm::Application.config.secret_token = 'ec85545e2ba21eb71ad5a0be981c92e5dc1147a06e091fe52c412f60619f8b7f3b07db365809d284a8f1b5db425c1aefb2d15f9b5485f097a9616c8e8b645a3d'
